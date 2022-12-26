4243b20238a359642a863c0b07c0dddf162453d4
Cristian Alejandro Arroayave <cristianalejo84@gmail.com>
2022-12-26 16:29:23 +0100

    Agregando crear receta

--- administrador.py
+++ administrador.py
@@ -14,7 +14,7 @@ def contar_recetas(ruta):
         contador += 1
     return contador
 
-# ? Funcion mostrar menu inicio
+# ? Menu inicio
 
 def inicio():
     # Vamos a limpiar la pantalla primero
@@ -39,7 +39,6 @@ def inicio():
             ''')
         eleccion_menu = input()
     return (eleccion_menu)
-        
 
 # ? Funcion mostrar categorias
 
@@ -48,12 +47,52 @@ def mostrar_categorias(ruta):
     ruta_categoria = Path(ruta)
     lista_categoria = []
     contador = 1
+    # Recorremos el directorio, con el metodo iterdir recorremos en carpetas
     for carpeta in ruta_categoria.iterdir():
+        # convertimos a string el nombre de la categoria
         carpeta_str = str(carpeta.name)
+        # Mostramos la categoria con el nombre
         print(f"[{contador}] - {carpeta_str}")
+        # Agregamos el directorio a un arreglo
         lista_categoria.append(carpeta)
         contador += 1
+    # Retornamos la lista
     return lista_categoria
+
+# ? Funcion elegir categoria
+
+def elegir_categoria(lista):
+    # Definimos una condicion dentro de un bucle
+    eleccion_correcta = 'x'
+    # Mientras no sea un numero o el numero no este dentro del rango ( se usa + 1 porque asi lo ve el usuario)
+    while not eleccion_correcta.isnumeric() or int(eleccion_correcta) not in range(1, len(lista) + 1):
+        eleccion_correcta = input("\nElige una categoria: ")
+    # Asi lo ve el usuario
+    return lista[int(eleccion_correcta) - 1]
+
+# ? Funcion mostrar recetas
+
+def mostrar_recetas(ruta):
+    print("Recetas:")
+    ruta_recetas = Path(ruta)
+    lista_recetas = []
+    contador = 1
+    for receta in ruta_recetas.glob('*.txt'):
+        receta_str = str(receta.name)
+        print(f"[{contador}] - {receta_str}")
+        lista_recetas.append(receta)
+        contador += 1
+    return lista_recetas
+
+# ? Funcion elegir receta
+
+def elegir_receta(lista):
+    elegir = 'x'
+    while not elegir.isnumeric() or int(elegir) not in range(1, len(lista) + 1):
+        elegir = input("Escoge una receta: ")
+    return lista[int(elegir) - 1]
+
+
 
 menu = 0
 
@@ -62,8 +101,11 @@ if menu == 1:
     # Mostrar Categorias
     mis_categorias = mostrar_categorias(mi_ruta)
     # Elegir categoria
+    mi_categoria = elegir_categoria(mis_categorias)
     # Mostrar recetas
+    mis_recetas = mostrar_recetas(mi_categoria)
     # Elegir recetas
+    mi_receta = elegir_receta(mis_recetas)
     # leer receta
     # Volver al inicio
     pass
@@ -73,6 +115,7 @@ elif menu == 2:
     # Mostrar Categorias
     mis_categorias = mostrar_categorias(mi_ruta)
     # Elegir categoria
+    mi_categoria = elegir_categoria(mis_categorias)
     # Crear Receta
     # Volver Inicio
     pass
@@ -88,8 +131,11 @@ elif menu == 4:
     # mostrar categorias
     mis_categorias = mostrar_categorias(mi_ruta)
     # elegir categoria
+    mi_categoria = elegir_categoria(mis_categorias)
     # mostrar recetas
+    mis_recetas = mostrar_recetas(mi_categoria)
     # elegir receta
+    mi_receta = elegir_receta(mis_recetas)
     # eliminar receta
     # volver a inicio
     pass
@@ -99,6 +145,7 @@ elif menu == 5:
     # mostrar categorias
     mis_categorias = mostrar_categorias(mi_ruta)
     # elegir categoria
+    mi_categoria = elegir_categoria(mis_categorias)
     # eliminar categoria
     # volver al inicio
     pass
